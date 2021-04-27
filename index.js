f97e605711b7898b0e99c26460fc8b5cbe0f6a58
eman hamid <emanhharki@gmail.com>
2021-04-27 19:12:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ class Lunch{
   }
 }
 const lunch =  new Lunch("tapula","chicken soup","cola");
-console.log(lunch.);
+console.log(lunch.getfood());
 class Dinner{
   constructor(salad, soup, entree,_dessert){
 
