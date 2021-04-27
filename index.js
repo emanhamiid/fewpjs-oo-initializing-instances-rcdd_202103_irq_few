988f955ed3eac92bb5685fc04a831068637faacf
eman hamid <emanhharki@gmail.com>
2021-04-27 19:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ class Dinner{
     this._dessert = _dessert;
   }
   getfood(){
-    return this.salad + " " + this.soup + " " + this.entree +" " + this.
+    return this.salad + " " + this.soup + " " + this.entree +" " + this._dessert;
   }
 }
 const dinner = new Dinner("salad","soup","entree","dessert");
